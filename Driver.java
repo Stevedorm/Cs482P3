4cba5835651735c7e6f6c13a5c8c4ab5a7cfbe77
Steve Dormady <dormadsa@dukes.jmu.edu>
2026-02-17 15:43:22 -0500

    compilied and clearing for version control

--- Driver.java
+++ Driver.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import java.util.*;
+
 public class Driver {
     // This was generated, not what we need exactly.
 
@@ -6,20 +9,63 @@ public class Driver {
     }
 
     public static void main (String[] args) {
+        String inText = "354C0FCABE7852DF42BC9DD6EAAB495CCB8B6158C93E2D5D2A49387717657ECEB6CAD9A517BD123AE58C720DF9CDFEA3B4132FBAE66DF6001A032BF627FC406B3F71931E4F818265157028D2212DAD85";
         // [32, 127) for characters decrypted, check each cahracter.
+        byte[] cipherText = inText.getBytes();
+        byte[] receivedIV = new byte[16];
+		for (int i = 0; i < 16; i++) receivedIV[i] = cipherText[i];
+        int num_blocks = (cipherText.length / 16) - 1 ;
+
         byte[] inKey = {(byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x60, (byte) 0x00,
 					(byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
 					(byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x03};
         //This is the IV: 354C0FCABE7852DF42BC9DD6EAAB495C
-        byte[] cbcIV = {
-            (byte) 0x35, (byte) 0x4C, (byte) 0x0F, (byte) 0xCA,
-            (byte) 0xBE, (byte) 0x78, (byte) 0x52, (byte) 0xDF,
-            (byte) 0x42, (byte) 0xBC, (byte) 0x9D, (byte) 0xD6,
-            (byte) 0xEA, (byte) 0xAB, (byte) 0x49, (byte) 0x5C
-        };
-        
-        while (true) {
-            if {}
+        boolean loop_bool = true;
+        while (loop_bool) {
+            // outer loop that continues until plaintext is found.
+                // inner loop that continues until a valid plaintext is found.
+                try {
+                    byte[] cipherTextLoop = new byte[cipherText.length];
+                    // for (int i=0; i < 16; i++) cipherTextLoop[i] = cbcIV[i];
+                    // for (int i=0; i < 16; i++) cipherTextLoop[i] = inKey[i];
+
+                    Object decryptRoundKeys = Rijndael_Algorithm.makeKey (Rijndael_Algorithm.DECRYPT_MODE, inKey); // 
+                    byte[] currentDecryptionBlock = new byte[16];
+                    byte[] cleartextBlocks = new byte[num_blocks * 16];
+
+                    boolean all_printable = true;
+                    for (int i=0; i < num_blocks; i++) {
+                        if (!all_printable) break;
+				        for (int j=0; j < 16; j++) currentDecryptionBlock [j] = cipherTextLoop[(i+1)*16 + j]; // Note that the first block is the IV
+
+				        byte[] thisDecryptedBlock = Rijndael_Algorithm.blockDecrypt2 (currentDecryptionBlock, 0, decryptRoundKeys);
+				        for (int j=0; j < 16; j++) cleartextBlocks[i*16+j] =  (byte) (thisDecryptedBlock[j] ^ cipherTextLoop[i*16 + j]);
+                        for (int j=0; j < 16; j++) {
+                            if (!isPrintableASCII(thisDecryptedBlock[j])) {
+                                all_printable = false;
+                                break;
+                            }
+                        }
+			        }
+                    
+                    if (all_printable) {
+                        System.out.println("Plaintext found: " + new String(cleartextBlocks));
+                        loop_bool = false;
+                        break;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                // increment the key
+                for (int i=15; i >= 0; i--) {
+                    if (inKey[i] == (byte) 0xFF) {
+                        inKey[i] = 0;
+                    } else {
+                        inKey[i]++;
+                        break;
+                    }
+                }
 
         }
             // try {
