b56f7f193502adc6da7328a3d2ef2ee4c6871827
Steve Dormady <dormadsa@dukes.jmu.edu>
2026-02-17 14:27:26 -0500

    key

--- Driver.java
+++ Driver.java
@@ -1,22 +1,32 @@
 public class Driver {
     // This was generated, not what we need exactly.
+
+    public static boolean isPrintableASCII (byte b) {
+        return b >= 32 && b < 127;
+    }
+    
     public static void main (String[] args) {
-        try {
-            String textString = "This is a plaintext message to be encrypted by AES algorithm. It can be of any length.";
-            byte[] inText = textString.getBytes();
+        // [32, 127) for characters decrypted, check each cahracter.
+        byte[] inKey = {(byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x60, (byte) 0x00,
+					(byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+					(byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x03};
 
-            byte[] inKey = new byte[16];
-            for (int i=0; i < 16; i++) inKey[i] = (byte) i;
+        // try {
+        //     String textString = "This is a plaintext message to be encrypted by AES algorithm. It can be of any length.";
+        //     byte[] inText = textString.getBytes();
 
-            byte[] cbcIV = new byte[16];
-            for (int i=0; i < 16; i++) cbcIV[i] = (byte) (i+1);
+        //     byte[] inKey = new byte[16];
+        //     for (int i=0; i < 16; i++) inKey[i] = (byte) i;
 
-            Object roundKeys = Rijndael_Algorithm.makeKey (Rijndael_Algorithm.ENCRYPT_MODE, inKey); // 
+        //     byte[] cbcIV = new byte[16];
+        //     for (int i=0; i < 16; i++) cbcIV[i] = (byte) (i+1);
 
-            int numOfBlocks = inText.length / 16;
-            if (inText.length % 16 != 0) numOfBlocks++; // We need to pad the last block if the plaintext is not a multiple of 16 bytes
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        //     Object roundKeys = Rijndael_Algorithm.makeKey (Rijndael_Algorithm.ENCRYPT_MODE, inKey); // 
+
+        //     int numOfBlocks = inText.length / 16;
+        //     if (inText.length % 16 != 0) numOfBlocks++; // We need to pad the last block if the plaintext is not a multiple of 16 bytes
+        // } catch (Exception e) {
+        //     e.printStackTrace();
+        // }
     }
 }
